122d08e81e5ac3b3ac8c5d91954c6295aa78d9b1
Cristóbal Molina <cris.molinab@gmail.com>
2020-09-19 03:18:29 -0300

    Modificacion CRUD Mesa
    Se agregan variables al Create de la Mesa

--- AppRestaurantSiglo21/Controllers/MesaController.cs
+++ AppRestaurantSiglo21/Controllers/MesaController.cs
@@ -16,7 +16,7 @@ namespace AppRestaurantSiglo21.Controllers
                                                                   // GET: TipoProducto
 
         // #################### LISTADO DE REGISTROS ################
-        public ActionResult IndexMesa()
+        public ActionResult Index()
         {
             var mesa = db.MESA.ToList(); //DEJA EN UNA VARIABLE TEMPORAL (COLECCIÓN) LO QUE OBTIENE DEL METODO TOLIST() DENTRO DEL OBJETO TIPOPRODUCTO
             return View(mesa); //RETORNA LA COLECCIÓN A LA VISTA INDEX 
@@ -26,18 +26,18 @@ namespace AppRestaurantSiglo21.Controllers
 
         // ############################ CREAR UN REGISTRO ################
         //ES NORMAL QUE ESTE METODO ESTE VACIO, PORQUE RECIBE LA ACCION A TRAVES DE HTTPPOST QUE ESTÁ MAS ABAJO
-        public ActionResult CreateMesa()
+        public ActionResult Create()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult CreateMesa(MESA objMesa) //REBICIÓ UN OBJETO BASADO EN EL MODELO DE TIPO TIPOPRODUCTO, DESDE LA VISTA
+        public ActionResult Create(MESA objMesa) //REBICIÓ UN OBJETO BASADO EN EL MODELO DE TIPO TIPOPRODUCTO, DESDE LA VISTA
         {
             if (ModelState.IsValid) //SI EL ESTADO DEL OBJETO ES VALIDO
             {
                 db.MESA.Add(objMesa); //SE INSTANCIA EL MAPEO DEL ENTITYFRAMEWORK PARA LA TABLA TIPOPRODUCTO, Y CON EL METODO ADD, SE LE PASA EL OBJETO
                 db.SaveChanges();
-                return RedirectToAction("IndexMesa"); //REDIRIGE LA ACCION AL METODO INDEX QUE LLEVA A LA VISTA POR DEFECTO DE LISTADO
+                return RedirectToAction("Index"); //REDIRIGE LA ACCION AL METODO INDEX QUE LLEVA A LA VISTA POR DEFECTO DE LISTADO
 
             }
             return View(objMesa);
@@ -45,7 +45,7 @@ namespace AppRestaurantSiglo21.Controllers
 
         // ############################ EDITAR UN REGISTRO ################
 
-        public ActionResult EditMesa(int? id) //SE TRAE ID DEL REGISTRO POR PARAMETRO
+        public ActionResult Edit(int? id) //SE TRAE ID DEL REGISTRO POR PARAMETRO
         {
             if (id == null)
             {
@@ -61,20 +61,20 @@ namespace AppRestaurantSiglo21.Controllers
             return View(objMesa); //RETORNA EL OBJETO EN CASO QUE HAYA ENCONTRADO UNA COINCIDENCIA
         }
         [HttpPost] //ESTO SUCEDE CUANDO LA CONTROLLER RECIBE UN POST AL METODO EDIT
-        public ActionResult EditMesa(MESA objMesa) //RECIBE EL OBJETO POR PARAMETROS 
+        public ActionResult Edit(MESA objMesa) //RECIBE EL OBJETO POR PARAMETROS 
         {
             if (ModelState.IsValid)
             {
                 db.Entry(objMesa).State = EntityState.Modified; //SI VIENE CON CAMBIOS, SE QUEDARÁ CON LOS CAMBIOS REALIZADOS
                 db.SaveChanges(); //CONSOLIDA EN LA BASE
-                return RedirectToAction("IndexMesa"); //REDIRIGE A LA VISTA DE LISTADO
+                return RedirectToAction("Index"); //REDIRIGE A LA VISTA DE LISTADO
             }
 
             return View(objMesa); //NO ENCONTRÓ COINCIDENCIAS NO RETORNA NADA
         }
 
         // ############################ ACCEDER AL DETALLE DE UN REGISTRO ################
-        public ActionResult DetailMesa(int? id)
+        public ActionResult Detail(int? id)
         {
             if (id == null)
             {
@@ -90,7 +90,7 @@ namespace AppRestaurantSiglo21.Controllers
         }
 
         // ############################ ELIMINAR UN REGISTRO ################
-        public ActionResult DeleteMesa(int? id)
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
@@ -105,12 +105,12 @@ namespace AppRestaurantSiglo21.Controllers
             return View(objMesa); //SI ENCUENTRA COINCIDENCIA RETORNARÁ EL OBJETO PARA MOSTRARLO EN LA VISTA DE DETALLE
         }
         [HttpPost]
-        public ActionResult DeleteMesa(int id)
+        public ActionResult Delete(int id)
         {
             var objMesa = db.MESA.SingleOrDefault(t => t.IDMESA == id);  //EN UNA VARIABLE SE ALMACENA EL RESULTADO DE LA QUERY ASOCIADA A LA TABLA TIPO DE PRODUCTO ES IGUAL AL ID QUE INGRESÓ POR PARAMETRO
             db.MESA.Remove(objMesa ?? throw new InvalidOperationException()); //REMUEVE EL REGISTRO DE LA BD DADO QUE EN LA LINEA ANTERIOR LO ENCONTRÓ
             db.SaveChanges(); //GUARDA CAMBIOS DE LA ELIMINACIÓN
-            return RedirectToAction("IndexMesa");  //REDIRIGE A LA VISTA DE LISTADO
+            return RedirectToAction("Index");  //REDIRIGE A LA VISTA DE LISTADO
         }
     }
 }
